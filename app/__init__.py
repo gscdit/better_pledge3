75b4be39cd6797ead60751a19dd1ec222d773dc5
Mayank Nader <mayank.nader11@gmail.com>
2019-01-10 23:40:06 +0530

    comment flask_apidoc

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from app import routes
 from flask import Flask
 from config import Config
 from flask_restful import Api
@@ -6,17 +7,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_admin import Admin
 from flask_migrate import Migrate
-from flask_apidoc import ApiDoc
+# from flask_apidoc import ApiDoc
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-doc = ApiDoc(app=app)
+# doc = ApiDoc(app=app)
 api = Api(app)
 migrate = Migrate(app, db)
 admin = Admin(app, name='Better Pledge', template_mode='bootstrap3')
 CORS(app)
-
-from app import routes

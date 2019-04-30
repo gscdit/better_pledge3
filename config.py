b83d02fb9be06a534e6bbfdc33fd974c9a65bd3a
Mayank Nader <mayank.nader11@gmail.com>
2019-04-30 23:49:54 +0530

    load configs from json file

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
-import os
+import json
+
+with open('/etc/betterpledge_config.json') as config_file:
+    config = json.load(config_file)
 
 
 class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secretkey'
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
+    SECRET_KEY = config.get('SECRET_KEY') or 'secretkey'
+    SQLALCHEMY_DATABASE_URI = config.get(
         'SQLALCHEMY_DATABASE_URI') or 'sqlite:///app.db'
     FLASK_ADMIN_SWATCH = 'cerulean'
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:rootroot@localhost:3307/db_test'
-    SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY') or 'your_api_key'
-    SENDGRID_DEFAULT_FROM = os.environ.get(
+    SENDGRID_API_KEY = config.get('SENDGRID_API_KEY') or 'your_api_key'
+    SENDGRID_DEFAULT_FROM = config.get(
         'SENDGRID_DEFAULT_FROM') or 'marketing@gscditu.com'
